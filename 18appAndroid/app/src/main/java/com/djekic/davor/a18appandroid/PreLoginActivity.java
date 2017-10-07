d7637fc61789c7ee14c952df322cee1219292864
DAVOR-PC\davor <davordjekic85@gmail.com>
2017-10-08 01:31:09 +0200

    Finito pre-login activity

--- 18appAndroid/app/src/main/java/com/djekic/davor/a18appandroid/PreLoginActivity.java
+++ 18appAndroid/app/src/main/java/com/djekic/davor/a18appandroid/PreLoginActivity.java
@@ -4,7 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
+import android.view.ViewGroup;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.ViewSwitcher;
 
 
 public class PreLoginActivity extends AppCompatActivity {
@@ -15,14 +20,73 @@ public class PreLoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pre_login);
 
-        Button btn = (Button) findViewById(R.id.buttonPreLogin);
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.relative_down);
+        layout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(PreLoginActivity.this, MainActivity.class);
                 startActivity(i);
             }
         });
+
+        //inizializza una imageView all'interno dell'imageSwitcher
+        final ImageSwitcher img = (ImageSwitcher) findViewById(R.id.imgSwitch);
+        img.setFactory(new ViewSwitcher.ViewFactory() {
+            public View makeView() {
+                ImageView imageView = new ImageView(getApplicationContext());
+                imageView.setLayoutParams(new ImageSwitcher.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+                return imageView;
+            }
+        });
+
+        //cambia immagine dell'imageSwitcher ogni 3 secondi
+        img.postDelayed(new Runnable() {
+            int i = 0;
+            ImageSwitcher image = (ImageSwitcher) findViewById(R.id.imgSwitch);
+            TextView txt = (TextView) findViewById(R.id.textEventi);
+            public void run() {
+                switch (i) {
+                    case 0:
+                        txt.setText("cinema");
+                        image.setImageResource(R.drawable.bg_cinema);
+                        i++;
+                        break;
+                    case 1:
+                        txt.setText("libri");
+                        image.setImageResource(R.drawable.bg_libri);
+                        i++;
+                        break;
+                    case 2:
+                        txt.setText("concerti");
+                        image.setImageResource(R.drawable.bg_concerti);
+                        i = 0;
+                }
+                img.postDelayed(this, 3000);
+            }
+        }, 0);
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*private void autoChange() {
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.relative_center);
+        Drawable drawable = res.getDrawable(R.drawable.bg_libri);
+        layout.setBackground(drawable);
+    }*/
 }
