74f1a171e39ad6f3a56a28d34789d49c4afbd440
Gerardo Doro <gerrydoro@live.it>
2017-10-08 01:34:13 +0200

    Sagra

---
+++ 18app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  18app
+//
+//  Created by Gerardo Doro on 07/10/17.
+//  Copyright © 2017 devs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
