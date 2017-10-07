fef5e6c3c947c7926b5361903dc519b3be5ff935
DAVOR-PC\davor <davordjekic85@gmail.com>
2017-10-07 23:57:22 +0200

    Risolto problema intent

--- 18appAndroid/app/src/main/java/com/djekic/davor/a18appandroid/MainActivity.java
+++ 18appAndroid/app/src/main/java/com/djekic/davor/a18appandroid/MainActivity.java
@@ -2,26 +2,25 @@ package com.djekic.davor.a18appandroid;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 
 
 
 public class MainActivity extends AppCompatActivity {
 
 
-    TextView txtMoney = (TextView)findViewById(R.id.txtMoney);
+    //TextView txtMoney = (TextView)findViewById(R.id.txtMoney);
 
-    int num;
+    //int num;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String a = txtMoney.getText().toString();
-        num = Integer.parseInt(a);
+
+        /*String a = txtMoney.getText().toString();
+        num = Integer.parseInt(a);*/
     }
 
 
-    ProgressBar pb = (ProgressBar)findViewById(R.id.progressBar);
+    //ProgressBar pb = (ProgressBar)findViewById(R.id.progressBar);
 
 }
--- 18appAndroid/app/src/main/java/com/djekic/davor/a18appandroid/PreLoginActivity.java
+++ 18appAndroid/app/src/main/java/com/djekic/davor/a18appandroid/PreLoginActivity.java
@@ -1,10 +1,10 @@
 package com.djekic.davor.a18appandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 
 public class PreLoginActivity extends AppCompatActivity {
@@ -20,13 +20,8 @@ public class PreLoginActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                /*Intent i = new Intent(PreLoginActivity.this, MainActivity.class);
-                startActivity(i);*/
-                Toast.makeText(
-                        getApplicationContext(),
-                        "Esempio toast breve",
-                        Toast.LENGTH_SHORT
-                ).show();
+                Intent i = new Intent(PreLoginActivity.this, MainActivity.class);
+                startActivity(i);
             }
         });
     }
