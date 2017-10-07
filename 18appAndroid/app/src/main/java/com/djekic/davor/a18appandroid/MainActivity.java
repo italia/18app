ff84009ae1a2df05775639d3862e6c87cdb980fb
Dario <carraroddario@gmail.com>
2017-10-07 23:18:58 +0200

    sistemato conflitti

---
+++ 18appAndroid/app/src/main/java/com/djekic/davor/a18appandroid/MainActivity.java
@@ -0,0 +1,27 @@
+package com.djekic.davor.a18appandroid;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    TextView txtMoney = (TextView)findViewById(R.id.txtMoney);
+
+    int num;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String a = txtMoney.getText().toString();
+        num = Integer.parseInt(a);
+    }
+
+
+    ProgressBar pb = (ProgressBar)findViewById(R.id.progressBar);
+
+}
--- 18appAndroid/app/src/main/java/com/djekic/davor/a18appandroid/PreLoginActivity.java
+++ 18appAndroid/app/src/main/java/com/djekic/davor/a18appandroid/PreLoginActivity.java
@@ -10,23 +10,15 @@ import android.widget.TextView;
 public class PreLoginActivity extends AppCompatActivity {
 
 
-    TextView txtMoney = (TextView)findViewById(R.id.txtMoney);
-
-    int num;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< HEAD:18appAndroid/app/src/main/java/com/djekic/davor/a18appandroid/MainActivity.java
-        setContentView(R.layout.activity_main);
-        String a = txtMoney.getText().toString();
-        num = Integer.parseInt(a);
 
-=======
         setContentView(R.layout.activity_pre_login);
->>>>>>> master:18appAndroid/app/src/main/java/com/djekic/davor/a18appandroid/PreLoginActivity.java
     }
 
 
-    ProgressBar pb = (ProgressBar)findViewById(R.id.progressBar);
+
+
 
 }
