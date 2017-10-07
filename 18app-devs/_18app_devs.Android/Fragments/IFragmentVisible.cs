a4198b29457bb89a65c8352f0c368c18b98bce27
Gerardo Doro <gerrydoro@live.it>
2017-10-07 18:08:11 +0200

    Eliminato

--- 18app-devs/_18app_devs.Android/Fragments/IFragmentVisible.cs
+++
@@ -1,7 +0,0 @@
-namespace _18app_devs.Droid
-{
-    interface IFragmentVisible
-    {
-        void BecameVisible();
-    }
-}
--- 18app-devs/_18app_devs/Interfaces/IBaseDataObject.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace _18app_devs.Interfaces
-{
-	public interface IBaseDataObject
-    {
-        string Id { get; set; }
-    }
-}
