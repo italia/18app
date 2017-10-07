11a98d690a5e49a092d9abbe522a7f0a524eca2a
lucafedri <luca.fedrizzi@belka.us>
2017-10-07 14:28:53 +0200

    Adding structure

--- App.js
+++ App.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+// import { Provider } from 'react-redux';
+// import createStore from './createStore'
+
+// import { Navigation } from 'react-native-navigation';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>asdfasdf</Text>
       </View>
     );
   }
 }
 
+const colors = {
+  main: '#0066cc'
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.main,
     alignItems: 'center',
     justifyContent: 'center',
   },
