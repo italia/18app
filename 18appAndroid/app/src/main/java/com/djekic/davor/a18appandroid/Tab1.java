24289753f25dd955ed9b0b4cfa9dddd2a4a6f2cc
Dario <carraroddario@gmail.com>
2017-10-08 13:13:43 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ 18appAndroid/app/src/main/java/com/djekic/davor/a18appandroid/Tab1.java
@@ -0,0 +1,21 @@
+package com.djekic.davor.a18appandroid;
+
+/**
+ * Created by Dario Carraro on 08/10/2017.
+ */
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Tab1 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab1, container, false);
+        return rootView;
+    }
+}
---
+++ 18appAndroid/app/src/main/java/com/djekic/davor/a18appandroid/Tab3.java
@@ -0,0 +1,20 @@
+package com.djekic.davor.a18appandroid;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Dario Carraro on 08/10/2017.
+ */
+
+public class Tab3  extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab3, container, false);
+        return rootView;
+    }
+}
