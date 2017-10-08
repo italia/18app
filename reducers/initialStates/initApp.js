98f86c619aaa16aa111d04c9173757ba74125271
lucafedri <luca.fedrizzi@belka.us>
2017-10-08 08:22:47 +0200

    Mocking faq

--- reducers/initialStates/initApp.js
+++ reducers/initialStates/initApp.js
@@ -58,6 +58,65 @@ const initialState = {
         selectedProductType: '',
         couponValue: 0,
         spent: false
+    },
+    faq: {
+        'free': [{
+            q: 'Contatti e Assistenza',
+            a: 'E\' una iniziativa a cura del Ministero dei Beni e delle Attività Culturali e del Turismo e della Presidenza del Consiglio dei Ministri dedicato a promuovere la cultura.Il programma, destinato a chi compie 18 anni nel 2016, permette loro di ottenere 500€ da spendere in cinema, concerti, eventi culturali, libri, musei, monumenti e parchi, teatro e danza. I ragazzi hanno tempo fino al 31 gennaio 2017 per registrarsi al Bonus Cultura e fino al 31 dicembre 2017 per spendere il bonus.'
+        }],
+        '18app e il Bonus Cultura': [{
+            q: 'Che cos’è 18app?',
+            a: 'E\' una iniziativa a cura del Ministero dei Beni e delle Attività Culturali e del Turismo e della Presidenza del Consiglio dei Ministri dedicato a promuovere la cultura.Il programma, destinato a chi compie 18 anni nel 2016, permette loro di ottenere 500€ da spendere in cinema, concerti, eventi culturali, libri, musei, monumenti e parchi, teatro e danza. I ragazzi hanno tempo fino al 31 gennaio 2017 per registrarsi al Bonus Cultura e fino al 31 dicembre 2017 per spendere il bonus.'
+        },
+        {
+            q: 'Che cos’è 18app?',
+            a: 'E\' una iniziativa a cura del Ministero dei Beni e delle Attività Culturali e del Turismo e della Presidenza del Consiglio dei Ministri dedicato a promuovere la cultura.Il programma, destinato a chi compie 18 anni nel 2016, permette loro di ottenere 500€ da spendere in cinema, concerti, eventi culturali, libri, musei, monumenti e parchi, teatro e danza. I ragazzi hanno tempo fino al 31 gennaio 2017 per registrarsi al Bonus Cultura e fino al 31 dicembre 2017 per spendere il bonus.'
+        },
+        {
+            q: 'Che cos’è il Bonus Cultura?',
+            a: 'E\' una iniziativa a cura del Ministero dei Beni e delle Attività Culturali e del Turismo e della Presidenza del Consiglio dei Ministri dedicato a promuovere la cultura.Il programma, destinato a chi compie 18 anni nel 2016, permette loro di ottenere 500€ da spendere in cinema, concerti, eventi culturali, libri, musei, monumenti e parchi, teatro e danza. I ragazzi hanno tempo fino al 31 gennaio 2017 per registrarsi al Bonus Cultura e fino al 31 dicembre 2017 per spendere il bonus.'
+        },
+        {
+            q: 'Chi può beneficiare del Bonus Cultura di 500€?',
+            a: 'E\' una iniziativa a cura del Ministero dei Beni e delle Attività Culturali e del Turismo e della Presidenza del Consiglio dei Ministri dedicato a promuovere la cultura.Il programma, destinato a chi compie 18 anni nel 2016, permette loro di ottenere 500€ da spendere in cinema, concerti, eventi culturali, libri, musei, monumenti e parchi, teatro e danza. I ragazzi hanno tempo fino al 31 gennaio 2017 per registrarsi al Bonus Cultura e fino al 31 dicembre 2017 per spendere il bonus.'
+        },
+        {
+            q: 'Come spendo il Bonus Cultura?',
+            a: 'E\' una iniziativa a cura del Ministero dei Beni e delle Attività Culturali e del Turismo e della Presidenza del Consiglio dei Ministri dedicato a promuovere la cultura.Il programma, destinato a chi compie 18 anni nel 2016, permette loro di ottenere 500€ da spendere in cinema, concerti, eventi culturali, libri, musei, monumenti e parchi, teatro e danza. I ragazzi hanno tempo fino al 31 gennaio 2017 per registrarsi al Bonus Cultura e fino al 31 dicembre 2017 per spendere il bonus.'
+        },
+        {
+            q: 'Come accedo a 18app??',
+            a: 'E\' una iniziativa a cura del Ministero dei Beni e delle Attività Culturali e del Turismo e della Presidenza del Consiglio dei Ministri dedicato a promuovere la cultura.Il programma, destinato a chi compie 18 anni nel 2016, permette loro di ottenere 500€ da spendere in cinema, concerti, eventi culturali, libri, musei, monumenti e parchi, teatro e danza. I ragazzi hanno tempo fino al 31 gennaio 2017 per registrarsi al Bonus Cultura e fino al 31 dicembre 2017 per spendere il bonus.'
+        },
+        {
+            q: 'Come mi registro a 18app?',
+            a: 'E\' una iniziativa a cura del Ministero dei Beni e delle Attività Culturali e del Turismo e della Presidenza del Consiglio dei Ministri dedicato a promuovere la cultura.Il programma, destinato a chi compie 18 anni nel 2016, permette loro di ottenere 500€ da spendere in cinema, concerti, eventi culturali, libri, musei, monumenti e parchi, teatro e danza. I ragazzi hanno tempo fino al 31 gennaio 2017 per registrarsi al Bonus Cultura e fino al 31 dicembre 2017 per spendere il bonus.'
+        },],
+        'SPID': [{
+            q: 'Che cos’è SPID?',
+            a: 'E\' una iniziativa a cura del Ministero dei Beni e delle Attività Culturali e del Turismo e della Presidenza del Consiglio dei Ministri dedicato a promuovere la cultura.Il programma, destinato a chi compie 18 anni nel 2016, permette loro di ottenere 500€ da spendere in cinema, concerti, eventi culturali, libri, musei, monumenti e parchi, teatro e danza. I ragazzi hanno tempo fino al 31 gennaio 2017 per registrarsi al Bonus Cultura e fino al 31 dicembre 2017 per spendere il bonus.'
+        },
+        {
+            q: 'A chi mi devo rivolgere e cosa devo fare per ottenere SPID?',
+            a: 'E\' una iniziativa a cura del Ministero dei Beni e delle Attività Culturali e del Turismo e della Presidenza del Consiglio dei Ministri dedicato a promuovere la cultura.Il programma, destinato a chi compie 18 anni nel 2016, permette loro di ottenere 500€ da spendere in cinema, concerti, eventi culturali, libri, musei, monumenti e parchi, teatro e danza. I ragazzi hanno tempo fino al 31 gennaio 2017 per registrarsi al Bonus Cultura e fino al 31 dicembre 2017 per spendere il bonus.'
+        },
+        {
+            q: 'Posso richiedere SPID se sono nel diciottesimo anno di età ma non ho ancora compiuto i 18 anni?',
+            a: 'E\' una iniziativa a cura del Ministero dei Beni e delle Attività Culturali e del Turismo e della Presidenza del Consiglio dei Ministri dedicato a promuovere la cultura.Il programma, destinato a chi compie 18 anni nel 2016, permette loro di ottenere 500€ da spendere in cinema, concerti, eventi culturali, libri, musei, monumenti e parchi, teatro e danza. I ragazzi hanno tempo fino al 31 gennaio 2017 per registrarsi al Bonus Cultura e fino al 31 dicembre 2017 per spendere il bonus.'
+
+        },
+        {
+            q: 'Ho perso le mie credenziali SPID, cosa devo fare per farmele rimandare?',
+            a: 'E\' una iniziativa a cura del Ministero dei Beni e delle Attività Culturali e del Turismo e della Presidenza del Consiglio dei Ministri dedicato a promuovere la cultura.Il programma, destinato a chi compie 18 anni nel 2016, permette loro di ottenere 500€ da spendere in cinema, concerti, eventi culturali, libri, musei, monumenti e parchi, teatro e danza. I ragazzi hanno tempo fino al 31 gennaio 2017 per registrarsi al Bonus Cultura e fino al 31 dicembre 2017 per spendere il bonus.'
+        },],
+        'Cosa acquistare': [{
+            q: 'Cosa posso acquistare attraverso 18app con il Bonus Cultura?',
+            a: 'E\' una iniziativa a cura del Ministero dei Beni e delle Attività Culturali e del Turismo e della Presidenza del Consiglio dei Ministri dedicato a promuovere la cultura.Il programma, destinato a chi compie 18 anni nel 2016, permette loro di ottenere 500€ da spendere in cinema, concerti, eventi culturali, libri, musei, monumenti e parchi, teatro e danza. I ragazzi hanno tempo fino al 31 gennaio 2017 per registrarsi al Bonus Cultura e fino al 31 dicembre 2017 per spendere il bonus.'
+        },
+        {
+            q: 'Posso acquistare CD o DVD con 18app?',
+            a: 'E\' una iniziativa a cura del Ministero dei Beni e delle Attività Culturali e del Turismo e della Presidenza del Consiglio dei Ministri dedicato a promuovere la cultura.Il programma, destinato a chi compie 18 anni nel 2016, permette loro di ottenere 500€ da spendere in cinema, concerti, eventi culturali, libri, musei, monumenti e parchi, teatro e danza. I ragazzi hanno tempo fino al 31 gennaio 2017 per registrarsi al Bonus Cultura e fino al 31 dicembre 2017 per spendere il bonus.'
+        },]
     }
 }
 
