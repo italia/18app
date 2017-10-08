a2dded359de457d4ed009d3da3e794037db85a19
Gerardo Doro <gerrydoro@live.it>
2017-10-08 15:47:46 +0200

    Tempo scaduto

---
+++ 18app/ViewSpid.swift
@@ -0,0 +1,25 @@
+//
+//  ViewSpid.swift
+//  18app
+//
+//  Created by Gerardo Doro on 07/10/17.
+//  Copyright © 2017 devs. All rights reserved.
+//
+
+import UIKit
+
+class ViewSpid: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
+
