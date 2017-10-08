7f49b192632b553685c59898aa5616f102e23eeb
Davor Djekic <davordjekic85@gmail.com>
2017-10-08 04:38:13 +0200

    Aggiunta InfoActivity

--- 18appAndroid/app/src/main/java/com/djekic/davor/a18appandroid/MainActivity.java
+++ 18appAndroid/app/src/main/java/com/djekic/davor/a18appandroid/MainActivity.java
@@ -1,8 +1,11 @@
 package com.djekic.davor.a18appandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -14,43 +17,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_navigation);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
 
         //Set new order as selected
         bottomNavigationView.setSelectedItemId(R.id.action_new);
 
-
-
-        //(bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener()
-        //)){
-            //@Override
-            /*public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId())
-                {
-                    case R.id.action_home:
-                        Intent i = new Intent(Orders.this, Home.class);
-                        i.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-                        startActivity(i);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_buoni:
+                        Intent intent0 = new Intent(MainActivity.this, BuoniActivity.class);
+                        intent0.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                        startActivity(intent0);
 
                         break;
-                    case R.id.action_new:
-                        Intent intent = new Intent(Orders.this, NewOrder.class);
-                        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-                        startActivity(intent);
+                    case R.id.action_shops:
+                        Intent intent1 = new Intent(MainActivity.this, MapsActivity.class);
+                        intent1.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                        startActivity(intent1);
+                        break;
+                    case R.id.action_info:
+                        Intent intent2 = new Intent(MainActivity.this, InfoActivity.class);
+                        intent2.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                        startActivity(intent2);
                         break;
                 }
                 return true;
-            }*/
-        //});
-
-
-
-
-
-
-
-
-
+            }
+        });
     }
 
 
