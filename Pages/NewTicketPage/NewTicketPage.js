ebec543cb5b925d03a68ce5893094a70bc93b609
Giuseppe Toto <giuseppe.toto@gmail.com>
2017-10-12 00:25:57 +0200

    fix nome corretto dell'Object Constructor "Categoria"

--- Pages/NewTicketPage/NewTicketPage.js
+++ Pages/NewTicketPage/NewTicketPage.js
@@ -1,23 +1,23 @@
 var Observable = require("FuseJS/Observable");
 
-function Catgoria(name, pathImg) {
+function Categoria(name, pathImg) {
 	this.name = name;
 	this.pathImg = pathImg;
 	
 }
 
-var catgorie = Observable(
-	new Catgoria("Cinema","Assets/images/icon-cinema-06c.png"),
-	new Catgoria("Concerti","Assets/images/icon-concerti-06c.png"),
-	new Catgoria("Eventi culturali","Assets/images/icon-eventi-06c.png"),
-	new Catgoria("Libri","Assets/images/icon-libri-06c.png"),
-	new Catgoria("Musei","Assets/images/icon-musei-06c.png"),
-	new Catgoria("Teatro e Danza","Assets/images/icon-teatro-06c.png")
+var categorie = Observable(
+	new Categoria("Cinema","Assets/images/icon-cinema-06c.png"),
+	new Categoria("Concerti","Assets/images/icon-concerti-06c.png"),
+	new Categoria("Eventi culturali","Assets/images/icon-eventi-06c.png"),
+	new Categoria("Libri","Assets/images/icon-libri-06c.png"),
+	new Categoria("Musei","Assets/images/icon-musei-06c.png"),
+	new Categoria("Teatro e Danza","Assets/images/icon-teatro-06c.png")
 );
 
 
 
 
 module.exports = {
-	catgorie: catgorie
+	categorie: categorie
 };
