2452ad5a4b54be2e9b169d1adbf28fe52f469ffe
Giuseppe Toto <giuseppe.toto@gmail.com>
2017-10-22 22:35:58 +0200

    MYCOUPON: Aggiunto campo prezzo

--- Pages/MyCouponPage/MyCouponPage.js
+++ Pages/MyCouponPage/MyCouponPage.js
@@ -4,6 +4,15 @@ var Context = require("Modules/Context");
 
 var items = Observable();
 
+var validItems = items.where({
+	state: "Valido"
+});
+
+
+var notValidItems = items.where(function(item) {
+	return item.state !== 'Valido';
+});
+
 Context.getCoupons().then(function(coupon) {
 	items.replaceAll(coupon);
 }).catch(function(e) {
@@ -13,9 +22,11 @@ Context.getCoupons().then(function(coupon) {
 var onTapItemList = function(args) {
 	router.push('newTicketStep2', {
 		macrocategory: args.data
-	}); 
+	});
 };
 module.exports = {
 	items: items,
+	validItems: validItems,
+	notValidItems:notValidItems,
 	onTapItemList: onTapItemList
 };
