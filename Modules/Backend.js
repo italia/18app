dece18b28ab26a27f0d1f8eee501d4f34035a2be
Giancarlo <giancarlo.valente@gmail.com>
2017-10-22 17:16:03 +0200

    informazioni raggruppate

--- Modules/Backend.js
+++ Modules/Backend.js
@@ -98,10 +98,37 @@ var macroCategories =
 }];
 
 var helpInformations = [
-{ id:'spid', title: "spid", description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro facilis esse repellendus pariatur cumque. Vel quis repellendus, molestiae mollitia culpa, voluptatem hic blanditiis ipsa dicta consequatur dolorem nesciunt iure, libero."},
-{ id:'testo2', title: "testo secondo elemento", description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro facilis esse repellendus pariatur cumque. Vel quis repellendus, molestiae mollitia culpa, voluptatem hic blanditiis ipsa dicta consequatur dolorem nesciunt iure, libero."},
-{ id:'testo3', title: "testo terzo elemento", description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro facilis esse repellendus pariatur cumque. Vel quis repellendus, molestiae mollitia culpa, voluptatem hic blanditiis ipsa dicta consequatur dolorem nesciunt iure, libero."},
+	{ 
+		group: '', 
+		items: [
+			{ id:'Contacts', group:'', title: "Contatti e Assistenza", description:" Assistenza tecnica: 800.991.199 da lunedì a venerdì esclusi festivi - dalle ore 09.00 alle 17.30 - Assistenza SPID: 06.82888.736 da lunedì a venerdì - dalle 9.30 alle 16.00 Helpdesk SPID "},
+		]
+	},
+	{ 
+		group: '18app e Bonus Cultura', 
+		items: [
+			{ id:'18app', title: "Cos'è 18app?", description:"18app è un’applicazione web che permette di gestire il Bonus Cultura di 500€ riservato a tutti i cittadini residenti in Italia (in possesso, ove previsto, di permesso di soggiorno in corso di validità) che compiono i diciotto anni di età nell’anno 2017. E’ realizzata per: • consentire agli esercenti di registrarsi al servizio ed offrire la vendita dei beni previsti dalla normativa ai diciottenni • consentire ai diciottenni di generare i buoni per l’acquisto dei beni offerti dagli esercenti registrati al servizio."},
+			{ id:'BonusCultura', title: "Cos'è il Bonus Cultura?", description:"E' un’iniziativa a cura del Ministero dei Beni e delle Attività Culturali e del Turismo e della Presidenza del Consiglio dei Ministri dedicata a promuovere la cultura. Il programma, destinato a chi compie 18 anni nel 2017, permette di ottenere 500€ da spendere in cinema, musica e concerti, eventi culturali, libri, musei, monumenti e parchi, teatro e danza, corsi di musica, di teatro o di lingua straniera. I ragazzi hanno tempo fino al 30 giugno 2018 per registrarsi a 18app e fino al 31 dicembre 2018 per spendere il Bonus Cultura."},
+			{ id:'BonusCulturaChi', title: "Chi può beneficiare del Bonus Cultura di 500€?", description:"Tutti i cittadini residenti nel territorio nazionale (in possesso, ove previsto, di permesso di soggiorno in corso di validità) che compiono i diciotto anni di età nell’anno 2017."},
+		]
+	},
+	{ 
+		group: 'SPID', 
+		items: [
+			{ id:'SPID', title: "Cos’è SPID?", description:"SPID (il Sistema Pubblico d’Identità Digitale) è la tua nuova identità digitale composta da credenziali (nome utente e password) che puoi richiedere dal giorno del tuo diciottesimo compleanno e che dovrai utilizzare tutte le volte che vorrai accedere a 18app. Maggiori informazioni sul sito spid.gov.it "},
+			{ id:'SPIDe18app', title: "Perché devo avere delle credenziali SPID per poter accedere a 18app?", description:"Perché dobbiamo essere sicuri che sei proprio tu a chiedere l’accesso in modo da permetterti di spendere in totale sicurezza il tuo bonus cultura. Le credenziali SPID che riceverai una volta fatta la richiesta sono uniche e potrai usarle soltanto tu. "},
+		]
+	},
+	{ 
+		group: 'Come acquistare', 
+		items: [
+			{ id:'BonusCulturaCosa', title: "Cosa posso acquistare attraverso 18app con il Bonus Cultura?", description:"Se sei nato nel 1999, puoi acquistare (entro dicembre 2018) beni per un totale di 500€, esclusivamente riconducibili ai seguenti ambiti: a. Cinema (abbonamento card/biglietto d’ingresso) b. Concerti (abbonamento card/biglietto d’ingresso) c. Eventi culturali (biglietto d’ingresso a festival, fiere culturali, circhi) d. Libri (audiolibro, ebook, libro) e. Musei, monumenti e parchi (abbonamento card/biglietto d’ingresso) f. Teatro e danza (abbonamento card/biglietto d’ingresso) g. Musica registrata (cd, dvd musicali, dischi in vinile, musica on line) h. Corsi di musica, di teatro o di lingua straniera. Se sei nato nel 1998, puoi acquistare (entro dicembre 2017) beni per un totale di 500€, esclusivamente riconducibili ai seguenti ambiti: a. Cinema (abbonamento card/biglietto d’ingresso) b. Concerti (abbonamento card/biglietto d’ingresso) c. Eventi culturali (biglietto d’ingresso a festival, fiere culturali, circhi) d. Libri (audiolibro, ebook, libro) e. Musei, monumenti e parchi (abbonamento card/biglietto d’ingresso) f. Teatro e danza (abbonamento card/biglietto d’ingresso) "},
+			{ id:'DVDeCD', title: "Posso acquistare CD o DVD con 18app?", description:"Sì, ma solo se sei nato nel 1999. E’ possibile acquistare solo dvd musicali, sono esclusi i dvd cinematografici."},
+		]
+	},
 ];
+
+
 
 var coupon ={
 	code:"45DH6789",
@@ -183,6 +210,7 @@ var getMicroCategories = function(idmacro) {
 var getHelpInformations = function() {
 	return new Promise(function(resolve, reject) {
 		setTimeout(function() {
+			//console.log("Debug inspect object: ", JSON.stringify(helpInformations, null, 4));
 			resolve(helpInformations);
 		}, 0);
 	});
--- Pages/InfoPage/InfoPage.js
+++ Pages/InfoPage/InfoPage.js
@@ -3,6 +3,7 @@ var Context = require("Modules/Context");
 var items = Observable();
 
 Context.getHelpInformations().then(function(helpInformations) {
+	// console.log(JSON.stringify(helpInformations, null, 4));
 	items.replaceAll(helpInformations);
 }).catch(function(e) {
 	console.log(error.message);
