b1ca6f38fb1b4c05e7d451cf2886c2732b436965
Giancarlo <giancarlo.valente@gmail.com>
2017-10-29 15:16:03 +0100

    fuzzy search sulle Faq

--- Pages/InfoPage/InfoPage.js
+++ Pages/InfoPage/InfoPage.js
@@ -1,13 +1,69 @@
 var Observable = require("FuseJS/Observable");
 var Context = require("Modules/Context");
+var FuseSearch = require('Libraries/fuse.min.js');
 var items = Observable();
 
+var delayTimer; // start search only when user stop typing
+var searchDelay = 600;
+var allHelpInformations = [];
+var fuseSearchOptions = {
+	  tokenize: true,
+	  matchAllTokens: true,
+	  findAllMatches: true,
+	  minMatchCharLength: 3,
+	  threshold: 0.1,
+	  location: 0,
+	  distance: 100,
+	  keys: ['items.title', 'items.description'],
+	  //keys: ['items.title'],
+	  id: 'items.id'
+};
+var fuseSearchEmptyCollection = [];
+var fuseSearch = new FuseSearch(fuseSearchEmptyCollection, fuseSearchOptions);
+
+function showAnyHelpInformations(){
+	console.log('Visualizzo tutto');
+	allHelpInformations.forEach(function(group){
+		group.items.forEach(function(item){
+			item.visible = true;
+		});
+		group.visible = true;
+	});
+	items.replaceAll(allHelpInformations);
+}
+
+function showOnlyThoseKeysHelpInformations(keys){
+	console.log("Fuzzy search ha trovato: ", JSON.stringify(keys));
+	allHelpInformations.forEach(function(group){
+		group.items.forEach(function(item){
+			item.visible = keys.includes(item.id);
+		});
+		group.visible = group.items.some(function(item) { return item.visible});
+	});
+	items.replaceAll(allHelpInformations);
+}
+
 Context.getHelpInformations().then(function(helpInformations) {
-	items.replaceAll(helpInformations);
+	allHelpInformations = helpInformations;
+	fuseSearch.setCollection(helpInformations);
+	showAnyHelpInformations();
 }).catch(function(e) {
 	console.log(error.message);
 });
 
+function onSearch(args) {
+	clearTimeout(delayTimer);
+	delayTimer = setTimeout(function(){
+		if (args.value === "") {
+			showAnyHelpInformations();
+			return;
+		} 
+		var keys = fuseSearch.search(args.value);
+		showOnlyThoseKeysHelpInformations(keys);
+	}, searchDelay);
+}
+
 module.exports = {
-	items: items
+	items: items,
+	onSearch: onSearch
 };
