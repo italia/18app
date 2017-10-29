786a72992b336db074be99b7494a228c369518ff
Giuseppe Toto <giuseppe.toto@gmail.com>
2017-10-29 19:49:38 +0100

    ora è possibile visualizzare un merchant locale su una mappa

--- Pages/MerchantPage/js/MerchantProfilePage.js
+++ Pages/MerchantPage/js/MerchantProfilePage.js
@@ -1,6 +1,8 @@
 var Observable = require("FuseJS/Observable");
 var Context = require("Modules/Context");
+var InterApp = require("FuseJS/InterApp");
 
+var Maps = require("FuseJS/Maps");
 
 var name = this.Parameter.map(function(param) {
 	return param.merchant.name;
@@ -15,34 +17,73 @@ var tag = this.Parameter.map(function(param) {
 });
 
 var street = this.Parameter.map(function(param) {
-	return param.merchant.address.street;
+	if(param.merchant.address!=null)
+		return param.merchant.address.street;
+	else
+		return null;
 });
 
 var postalCode = this.Parameter.map(function(param) {
-	return param.merchant.address.postalCode;
+	if(param.merchant.address!=null)
+		return param.merchant.address.postalCode;
+	else
+		return null;
 });
 
 var city = this.Parameter.map(function(param) {
-	return param.merchant.address.city;
+	if(param.merchant.address!=null)
+		return param.merchant.address.city;
+	else
+		return null;
 });
 
 var state = this.Parameter.map(function(param) {
-	return param.merchant.address.state;
+	if(param.merchant.address!=null)
+		return param.merchant.address.state;
+	else 
+		return null;
+});
+
+var gps = this.Parameter.map(function(param) {
+	if(param.merchant.address!=null){
+		return param.merchant.address.gps;
+	}
+	else
+		return null;
 });
 
 var type = this.Parameter.map(function(param) {
 	return param.merchant.type;
 });
 
+var site = this.Parameter.map(function(param){
+	return param.merchant.site;
+});
+
+function OpenSite(){
+	if(site.value!=null)
+		InterApp.launchUri(site.value);
+}
+
+function OpenMap(){
+	if(gps.value!=null){
+		console.log("OPEN MAP",gps.value.lat,gps.value.lng);
+		Maps.searchNear(gps.value.lat, gps.value.lng, name.value);
+	}
+}
 
 
 module.exports = {
 	name:name,
 	tag:tag,
+	gps:gps,
 	cover:cover,
 	street:street,
 	postalCode:postalCode,
 	city:city,
+	site:site,
 	state:state,
-	type:type
+	type:type,
+	OpenMap:OpenMap,
+	OpenSite:OpenSite
 };
