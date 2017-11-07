4cc27b79c30f769ca867b91ee76eaf79c807816d
Mario Robert D'Ambrosio <MarioRD@users.noreply.github.com>
2017-11-07 16:29:44 +0100

    Add files via upload

--- 18AppCustomer/app/build/generated/source/r/debug/android/support/transition/R.java
+++ 18AppCustomer/app/build/generated/source/r/debug/android/support/transition/R.java
@@ -8,7 +8,7 @@ package android.support.transition;
 
 public final class R {
     public static final class id {
-        public static final int transition_current_scene = 0x7f0d000b;
-        public static final int transition_scene_layoutid_cache = 0x7f0d000c;
+        public static final int transition_current_scene = 0x7f0900fc;
+        public static final int transition_scene_layoutid_cache = 0x7f0900fd;
     }
 }
--- 18AppCustomer/app/build/generated/source/r/debug/android/support/v7/recyclerview/R.java
+++ 18AppCustomer/app/build/generated/source/r/debug/android/support/v7/recyclerview/R.java
@@ -8,26 +8,26 @@ package android.support.v7.recyclerview;
 
 public final class R {
     public static final class attr {
-        public static final int layoutManager = 0x7f010113;
-        public static final int reverseLayout = 0x7f010115;
-        public static final int spanCount = 0x7f010114;
-        public static final int stackFromEnd = 0x7f010116;
+        public static final int layoutManager = 0x7f0300c6;
+        public static final int reverseLayout = 0x7f030138;
+        public static final int spanCount = 0x7f03014a;
+        public static final int stackFromEnd = 0x7f030150;
     }
     public static final class dimen {
-        public static final int item_touch_helper_max_drag_scroll_per_frame = 0x7f08007d;
-        public static final int item_touch_helper_swipe_escape_max_velocity = 0x7f08007e;
-        public static final int item_touch_helper_swipe_escape_velocity = 0x7f08007f;
+        public static final int item_touch_helper_max_drag_scroll_per_frame = 0x7f060080;
+        public static final int item_touch_helper_swipe_escape_max_velocity = 0x7f060081;
+        public static final int item_touch_helper_swipe_escape_velocity = 0x7f060082;
     }
     public static final class id {
-        public static final int item_touch_helper_previous_elevation = 0x7f0d0005;
+        public static final int item_touch_helper_previous_elevation = 0x7f09005e;
     }
     public static final class styleable {
-        public static final int[] RecyclerView = { 0x010100c4, 0x010100f1, 0x7f010113, 0x7f010114, 0x7f010115, 0x7f010116 };
-        public static final int RecyclerView_android_descendantFocusability = 1;
+        public static final int[] RecyclerView = { 0x010100c4, 0x010100f1, 0x7f0300c6, 0x7f030138, 0x7f03014a, 0x7f030150 };
         public static final int RecyclerView_android_orientation = 0;
+        public static final int RecyclerView_android_descendantFocusability = 1;
         public static final int RecyclerView_layoutManager = 2;
-        public static final int RecyclerView_reverseLayout = 4;
-        public static final int RecyclerView_spanCount = 3;
+        public static final int RecyclerView_reverseLayout = 3;
+        public static final int RecyclerView_spanCount = 4;
         public static final int RecyclerView_stackFromEnd = 5;
     }
 }
