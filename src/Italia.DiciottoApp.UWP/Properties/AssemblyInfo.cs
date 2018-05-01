6cabe6406acdff8c798b0b3fd028a88891b22fa4
Nicolò Carandini <n.carandini@outlook.com>
2018-05-01 13:54:22 +0200

    Create first hello 18App
    Related Work Items: #217

--- src/Italia.DiciottoApp.UWP/Properties/AssemblyInfo.cs
+++ src/Italia.DiciottoApp.UWP/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Italia.DiciottoApp.UWP")]
+[assembly: AssemblyTitle("18App")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Developers Italia")]
 [assembly: AssemblyProduct("Italia.DiciottoApp.UWP")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2018 Nicolò Carandini")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
