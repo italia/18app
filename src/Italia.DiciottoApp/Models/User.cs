2c39948d57ceaba7aff91252b8869d11b8bd901c
Nicolò Carandini <n.carandini@outlook.com>
2018-06-05 10:56:14 +0200

    Add minimal user model

---
+++ src/Italia.DiciottoApp/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Italia.DiciottoApp.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+    }
+}
