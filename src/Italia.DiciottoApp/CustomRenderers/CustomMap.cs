0a81b30a3f87ecaad7d5ee95a524857ff793ae36
Nicolò Carandini <n.carandini@outlook.com>
2018-06-23 15:31:34 +0200

    WIP Map and pin custom rederer

---
+++ src/Italia.DiciottoApp/CustomRenderers/CustomMap.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms.Maps;
+
+namespace Italia.DiciottoApp.CustomRenderers
+{
+    public class CustomMap : Map
+    {
+        public List<CustomPin> CustomPins { get; set; }
+    }
+}
