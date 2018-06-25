89c38289804ae1010148fdfe83f86cf199d50e32
Nicolò Carandini <n.carandini@outlook.com>
2018-06-25 07:11:35 +0200

    Add Coupon Model
    Related Work Items: #258

---
+++ src/Italia.DiciottoApp/Models/Coupon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Italia.DiciottoApp.Models
+{
+    public class Coupon
+    {
+        public string Id { get; set; }
+
+        public bool Spent { get; set; }
+
+        public string ShopId { get; set; }
+
+        public Categoria Category { get; set; }
+
+        public Prodotto Product { get; set; }
+
+        public decimal Value { get; set; }
+
+        public Shop Shop { get; set; }
+    }
+}
