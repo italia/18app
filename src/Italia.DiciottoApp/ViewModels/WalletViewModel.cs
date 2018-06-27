92e103bbbc1551ba396247aa9f7ce7dc9b652362
Nicolò Carandini <n.carandini@outlook.com>
2018-06-28 01:01:46 +0200

    WIP WalletPage

--- src/Italia.DiciottoApp/ViewModels/WalletViewModel.cs
+++ src/Italia.DiciottoApp/ViewModels/WalletViewModel.cs
@@ -1,12 +1,15 @@
-﻿
-using Italia.DiciottoApp.Models;
+﻿using Italia.DiciottoApp.Models;
+using Italia.DiciottoApp.Services;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Italia.DiciottoApp.ViewModels
 {
-    public class WalletViewModel : BaseViewModel
+    class WalletViewModel : BaseViewModel
     {
         #region Properties
 
@@ -14,11 +17,157 @@ namespace Italia.DiciottoApp.ViewModels
 
         public AppArea AppArea => AppArea.Wallet;
 
+        public bool IsAllTabSelected => WalletKind == WalletKind.All;
+
+        public bool IsAvailableTabSelected => WalletKind == WalletKind.All;
+
+        public bool IsSpentTabSelected => WalletKind == WalletKind.All;
+
+        public int WalletTabIndex => (int)WalletKind;
+
+        private WalletKind walletKind;
+        public WalletKind WalletKind
+        {
+            get => walletKind;
+            set => SetProperty(ref walletKind, value, onChanged: () =>
+            {
+                OnPropertyChanged(nameof(IsAllTabSelected));
+                OnPropertyChanged(nameof(IsAvailableTabSelected));
+                OnPropertyChanged(nameof(IsSpentTabSelected));
+                OnPropertyChanged(nameof(WalletTabIndex));
+            });
+        }
+
+        public bool CouponListIsVisible => (Coupons != null && Coupons.Count > 0 && !IsBusy);
+
+        public ObservableCollection<Coupon> Coupons { get; set; } = new ObservableCollection<Coupon>();
+
+        private string contentHeader;
+        public string ContentHeader
+        {
+            get => contentHeader;
+            set => SetProperty(ref contentHeader, value);
+        }
+
+
         #endregion
 
         public WalletViewModel() : base()
         {
+            ContentHeader = "Richiesta buoni in corso...";
         }
+
+        public async Task SetTab(WalletKind walletKind)
+        {
+            if (IsBusy)
+            {
+                return;
+            }
+
+            WalletKind = walletKind;
+            IsBusy = true;
+            OnPropertyChanged(nameof(CouponListIsVisible));
+            ContentHeader = "Richiesta buoni in corso...";
+            var couponService = Service.Resolve<ICouponsService>();
+
+            IEnumerable<Coupon> availableCoupons = null;
+            if (WalletKind == WalletKind.All || WalletKind == WalletKind.Available)
+            {
+                availableCoupons = await couponService.GetUserCouponsAsync(Settings.UserId, WalletKind.Available);
+            }
+
+            IEnumerable<Coupon> spentCoupons = null;
+            if (WalletKind == WalletKind.All || WalletKind == WalletKind.Spent)
+            {
+                spentCoupons = await couponService.GetUserCouponsAsync(Settings.UserId, WalletKind.Spent);
+            }
+
+            Coupons.Clear();
+            if (availableCoupons != null)
+            {
+                foreach (var coupon in availableCoupons)
+                {
+                    Coupons.Add(coupon);
+                }
+            }
+
+            if (spentCoupons != null)
+            {
+                foreach (var coupon in spentCoupons)
+                {
+                    Coupons.Add(coupon);
+                }
+            }
+
+            if (WalletKind == WalletKind.All && Coupons.Count == 0)
+            {
+                ContentHeader = $"La ricerca non ha restituito alcun buono";
+            }
+            else
+            {
+                switch (WalletKind)
+                {
+                    case WalletKind.All:
+                        switch (availableCoupons.Count())
+                        {
+                            case 0:
+                                ContentHeader = $"Non ci sono buoni ancora da spendere.";
+                                break;
+                            case 1:
+                                ContentHeader = $"C'è un buono ancora da spendere.";
+                                break;
+                            default:
+                                ContentHeader = $"Ci sono {availableCoupons.Count()} buoni ancora da spendere.";
+                                break;
+                        }
+                        break;
+                    case WalletKind.Available:
+                        switch (availableCoupons.Count())
+                        {
+                            case 0:
+                                ContentHeader = $"La ricerca non ha restituito alcun buono da spendere.";
+                                break;
+                            case 1:
+                                ContentHeader = $"C'è un buono ancora da spendere";
+                                break;
+                            default:
+                                ContentHeader = $"Ci sono {availableCoupons.Count()} buoni ancora da spendere";
+                                break;
+                        }
+                        break;
+                    case WalletKind.Spent:
+                        switch (spentCoupons.Count())
+                        {
+                            case 0:
+                                ContentHeader = $"La ricerca non ha restituito alcun buono già speso.";
+                                break;
+                            case 1:
+                                ContentHeader = $"Hai un buono già speso";
+                                break;
+                            default:
+                                ContentHeader = $"Hai {spentCoupons.Count()} buoni già spesi";
+                                break;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                
+            }
+
+            IsBusy = false;
+            OnPropertyChanged(nameof(CouponListIsVisible));
+        }
+
+        //public override void OnDisappearing()
+        //{
+        //    if (IsBusy)
+        //    {
+        //        if (cts != null && !cts.IsCancellationRequested)
+        //            cts.Cancel();
+        //    }
+        //    base.OnDisappearing();
+        //}
 
     }
 }
--- src/Italia.DiciottoApp/Views/WalletPage.xaml.cs
+++ src/Italia.DiciottoApp/Views/WalletPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Italia.DiciottoApp.ViewModels;
+﻿using Italia.DiciottoApp.Models;
+using Italia.DiciottoApp.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,11 +16,55 @@ namespace Italia.DiciottoApp.Views
     {
         private WalletViewModel vm;
 
-        public WalletPage ()
-		{
-			InitializeComponent ();
+        public WalletPage()
+        {
+            InitializeComponent();
             NavigationPage.SetHasNavigationBar(this, false);
             vm = BindingContext as WalletViewModel;
         }
-	}
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await vm.SetTab(WalletKind.All);
+        }
+
+        private async void OnAllTabTapped(object sender, EventArgs e)
+        {
+            if (vm.WalletKind != WalletKind.All)
+            {
+                await vm.SetTab(WalletKind.All);
+            }
+        }
+
+        private async void OnAvailableTabTapped(object sender, EventArgs e)
+        {
+            if (vm.WalletKind != WalletKind.Available)
+            {
+                await vm.SetTab(WalletKind.Available);
+            }
+        }
+
+        private async void OnSpentTabTapped(object sender, EventArgs e)
+        {
+            if (vm.WalletKind != WalletKind.Spent)
+            {
+                await vm.SetTab(WalletKind.Spent);
+            }
+        }
+
+        private async void OnListViewItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            if (e.Item is Coupon coupon)
+            {
+                // Clear the item selection
+                if (sender is ListView listView)
+                {
+                    listView.SelectedItem = null;
+                }
+
+                await Navigation.PushAsync(new CouponPage(coupon, justCreated: false));
+            }
+        }
+    }
 }
