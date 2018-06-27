18f1af750393d3065145ed8559c46e0bab8db14f
Nicolò Carandini <n.carandini@outlook.com>
2018-06-28 00:49:38 +0200

    Add NegativeConverter

---
+++ src/Italia.DiciottoApp/Converters/NegativeConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Italia.DiciottoApp.Converters
+{
+    public class NegativeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool v)
+                return !v;
+            else
+                return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool v)
+                return !v;
+            else
+                return true;
+        }
+    }
+}
