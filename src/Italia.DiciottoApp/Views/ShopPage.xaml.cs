7ab29ea4621014a2567a2ba656c5ac1b14ffa8dc
Nicolò Carandini <n.carandini@outlook.com>
2018-07-03 20:33:55 +0200

    Create shop page
    Related Work Items: #268

---
+++ src/Italia.DiciottoApp/Views/ShopPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Italia.DiciottoApp.Models;
+using Italia.DiciottoApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Italia.DiciottoApp.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ShopPage : BasePage
+    {
+        private ShopViewModel vm;
+
+        public ShopPage(Shop shop)
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+            vm = BindingContext as ShopViewModel;
+            vm.Shop = shop;
+        }
+
+        private void OnShowMapButtonTapped(object sender, EventArgs e)
+        {
+            // TODO
+        }
+
+        private void OnRouteToShopButtonTapped(object sender, EventArgs e)
+        {
+            // TODO
+        }
+
+        private void OnGotoOnlineButtonTapped(object sender, EventArgs e)
+        {
+            // TODO
+        }
+
+    }
+}
