ac8e3b98ef87ec3f8f6429884f1344e24c19fb7e
Nicolò Carandini <n.carandini@outlook.com>
2018-07-14 15:34:23 +0200

    Add InfoTextIconConverter
    Related Work Items: #280

---
+++ src/Italia.DiciottoApp/Converters/InfoTextIconConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Italia.DiciottoApp.Converters
+{
+    public class InfoTextIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is bool))
+            {
+                throw new ArgumentException("value is not a boolean");
+            }
+            if ((bool)value == true)
+            {
+                return "down";
+            }
+            else
+            {
+                return "forward";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
