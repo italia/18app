b32a40196f21744926e5561c02e0e4f95088cda0
Nicolò Carandini <n.carandini@outlook.com>
2018-07-17 13:45:52 +0200

    Set Assembly version to 1.0.*

--- src/Italia.DiciottoApp.Android/Properties/AssemblyInfo.cs
+++ src/Italia.DiciottoApp.Android/Properties/AssemblyInfo.cs
@@ -26,7 +26,7 @@ using Android.App;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
 // Add some common permissions, these can be removed if not needed
@@ -39,3 +39,10 @@ using Android.App;
 [assembly: UsesFeature("android.hardware.location", Required = false)]
 [assembly: UsesFeature("android.hardware.location.gps", Required = false)]
 [assembly: UsesFeature("android.hardware.location.network", Required = false)]
+
+// Disable debugging on Release version
+#if DEBUG
+[assembly: Application(Debuggable=true)]
+#else
+[assembly: Application(Debuggable = false)]
+#endif
