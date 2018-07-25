074a0f4ab6f2b0973e9a776d62f965f701beb312
Nicolò Carandini <n.carandini@outlook.com>
2018-07-25 08:39:38 +0200

    Add Municipality model

---
+++ src/Italia.DiciottoApp/Models/Municipality.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Italia.DiciottoApp.Models
+{
+    public class Municipality
+    {
+        public string Id { get; set; }
+
+        public string RegionId { get; set; }
+
+        public string Province => Id.Substring(0, 3);
+
+        public string Name { get; set; }
+    }
+}
