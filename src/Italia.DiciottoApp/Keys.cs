512f387acce2a2565dbdaddad9ebbf1769fc28c3
Nicolò Carandini <n.carandini@outlook.com>
2018-11-22 10:56:19 +0100

    Add Keys file with fake vaules

---
+++ src/Italia.DiciottoApp/Keys.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Italia.DiciottoApp
+{
+    public static class Keys
+    {
+        // Fake keys
+        public static readonly string X_IBM_ClientId = "FakeClientId";
+        public static readonly string X_IBM_ClientSecret = "FakeClientSecret";
+    }
+}
