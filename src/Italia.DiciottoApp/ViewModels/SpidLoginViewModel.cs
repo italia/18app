697a5f0cdca887befeaa4d4c96731f046069b82a
Nicolò Carandini <n.carandini@outlook.com>
2018-12-15 20:24:48 +0100

    Refactor Test and Prod IdP login

--- src/Italia.DiciottoApp/ViewModels/SpidLoginViewModel.cs
+++ src/Italia.DiciottoApp/ViewModels/SpidLoginViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading.Tasks;
 using Italia.DiciottoApp.Models;
 
 namespace Italia.DiciottoApp.ViewModels
@@ -19,26 +20,5 @@ namespace Italia.DiciottoApp.ViewModels
         {
         }
 
-        public void Login(IdP idp)
-        {
-            // TODO: call login service passing the idp parameter
-
-            // TODO: remove fake succes code:
-            Settings.UserLogged = true;
-            Settings.UserId = Guid.NewGuid().ToString();
-            Settings.UserName = "Franco";
-            Settings.UserSurname = "Rossi";
-            Settings.UserCodFisc = "RSSFNC00H22H501G";
-            Settings.UserEmail = "franco.rossi@testdomain.it";
-            Settings.UserResidenceAddress = "Via della Repubblica, 171";
-            Settings.UserResidenceZip = "31100";
-            Settings.UserResidenceCity = "Treviso";
-            Settings.UserResidenceProvince = "TV";
-            Settings.UserBirthDate = new DateTime(2000, 6, 22);
-            Settings.UserBirthCity = "Roma";
-            Settings.UserBirthProvince = "RM";
-            Settings.UserPhoneNumber = "333 1234567";
-            Settings.UserSpidIdPIndex = (int)idp;
-        }
     }
 }
--- src/Italia.DiciottoApp/Views/SpidLoginPage.xaml.cs
+++ src/Italia.DiciottoApp/Views/SpidLoginPage.xaml.cs
@@ -23,67 +23,44 @@ namespace Italia.DiciottoApp.Views
             vm = BindingContext as SpidLoginViewModel;
         }
 
-        private async Task Login(IdP idp)
-        {
-            // TODO: Fix this when fake login will be removed
-            vm.Login(idp);
-
-            if (Settings.UserLogged)
-            {
-                // Get the root page
-                IReadOnlyList<Page> navStack = Navigation.NavigationStack;
-                Page currentRootPage = navStack[0];
-
-                // Insert page before WelcomePage
-                Navigation.InsertPageBefore(new LoggedRootPage(), currentRootPage);
-            }
-            else
-            {
-                // Don't do anything, we'll go to the root page that is already the Welcome page
-            }
-
-            // Clear navigation stack
-            await Navigation.PopToRootAsync();
-        }
-
         private async void OnIdpArubaTapped(object sender, EventArgs e)
         {
-            await Login(IdP.Aruba);
+            await Navigation.PushAsync(new LoginWebPage(IdP.Aruba));
         }
 
         private async void OnIdpInfoCertTapped(object sender, EventArgs e)
         {
-            await Login(IdP.InfoCert);
+            await Navigation.PushAsync(new LoginWebPage(IdP.InfoCert));
         }
 
         private async void OnIdpIntesaTapped(object sender, EventArgs e)
         {
-            await Login(IdP.Intesa);
+            await Navigation.PushAsync(new LoginWebPage(IdP.Intesa));
         }
 
         private async void OnIdpNamirialTapped(object sender, EventArgs e)
         {
-            await Login(IdP.Namirial);
+            await Navigation.PushAsync(new LoginWebPage(IdP.Namirial));
         }
 
         private async void OnIdpPosteTapped(object sender, EventArgs e)
         {
-            await Login(IdP.Poste);
+            await Navigation.PushAsync(new LoginWebPage(IdP.Poste));
         }
 
         private async void OnIdpRegisterTapped(object sender, EventArgs e)
         {
-            await Login(IdP.Register);
+            await Navigation.PushAsync(new LoginWebPage(IdP.Register));
         }
 
         private async void OnIdpSielteTapped(object sender, EventArgs e)
         {
-            await Login(IdP.Sielte);
+            await Navigation.PushAsync(new LoginWebPage(IdP.Sielte));
         }
 
         private async void OnIdpTimTapped(object sender, EventArgs e)
         {
-            await Login(IdP.Tim);
+            await Navigation.PushAsync(new LoginWebPage(IdP.Tim));
         }
 
         private async void TestIdpButton_Clicked(object sender, EventArgs e)
