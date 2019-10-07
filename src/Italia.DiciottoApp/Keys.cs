1898751ca687df26bd789ba5135ed5ba6af72838
Nicolò Carandini <ncarandini@users.noreply.github.com>
2019-10-07 10:52:04 +0200

    Create fake Keys.cs file

---
+++ src/Italia.DiciottoApp/Keys.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Italia.DiciottoApp
+{
+    public static class Keys
+    {
+        public static readonly bool IsInstabugEnabled = true;
+        public static readonly string InstabugToken = "InstabugToken";
+
+        public static string X_IBM_ClientId_ProdEnv => "X_IBM_ClientId_ProdEnv";
+        public static string X_IBM_ClientId_TestEnv => "X_IBM_ClientId_TestEnv";
+
+        public static string X_IBM_ClientSecret_ProdEnv => "X_IBM_ClientSecret_ProdEnv";
+        public static string X_IBM_ClientSecret_TestEnv => "X_IBM_ClientSecret_TestEnv";
+
+    }
+}
