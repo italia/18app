5b917a8141ed274797fd794a798ceffc228da0bb
Nicolò Carandini <n.carandini@outlook.com>
2019-12-16 08:39:18 +0100

    Fix local datetime issue

--- src/Italia.DiciottoApp/Utils/EpochConverter.cs
+++ src/Italia.DiciottoApp/Utils/EpochConverter.cs
@@ -8,6 +8,7 @@ namespace Italia.DiciottoApp.Utils
     public class EpochConverter : DateTimeConverterBase
     {
         private static readonly DateTime epochDateTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        private static readonly TimeSpan localBaseUtcOffset = new TimeSpan(1, 0, 0);
 
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
@@ -21,8 +22,8 @@ namespace Italia.DiciottoApp.Utils
                 return null;
             }
             Debug.WriteLine($"### Backend  ### --> Epoch value: {(long)reader.Value}");
-            Debug.WriteLine($"### Frontend ### --> Date value : {epochDateTime.AddMilliseconds((long)reader.Value).ToString("dd MMMM yyyy")}");
-            return epochDateTime.AddMilliseconds((long)reader.Value);
+            Debug.WriteLine($"### Frontend ### --> Date value : {epochDateTime.AddMilliseconds((long)reader.Value).Add(localBaseUtcOffset).ToString("dd MMMM yyyy")}");
+            return epochDateTime.AddMilliseconds((long)reader.Value).Add(localBaseUtcOffset);
         }
     }
 }
