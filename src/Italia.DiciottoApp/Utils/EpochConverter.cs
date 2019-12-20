e4edb8eabc027c38dd34f522447ca97a5104c782
nick <n.carandini@outlook.com>
2019-12-20 17:39:10 +0100

    Fix EpochConverter

--- src/Italia.DiciottoApp/Utils/EpochConverter.cs
+++ src/Italia.DiciottoApp/Utils/EpochConverter.cs
@@ -8,7 +8,7 @@ namespace Italia.DiciottoApp.Utils
     public class EpochConverter : DateTimeConverterBase
     {
         private static readonly DateTime epochDateTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-        private static readonly TimeSpan localBaseUtcOffset = new TimeSpan(1, 0, 0);
+        private static readonly TimeSpan localBaseUtcOffset = new TimeSpan(2, 0, 0);
 
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
@@ -22,7 +22,7 @@ namespace Italia.DiciottoApp.Utils
                 return null;
             }
             Debug.WriteLine($"### Backend  ### --> Epoch value: {(long)reader.Value}");
-            Debug.WriteLine($"### Frontend ### --> Date value : {epochDateTime.AddMilliseconds((long)reader.Value).Add(localBaseUtcOffset).ToString("dd MMMM yyyy")}");
+            Debug.WriteLine($"### Frontend ### --> Date value : {epochDateTime.AddMilliseconds((long)reader.Value).Add(localBaseUtcOffset).ToString("dd MMMM yyyy hh:mm")}");
             return epochDateTime.AddMilliseconds((long)reader.Value).Add(localBaseUtcOffset);
         }
     }
